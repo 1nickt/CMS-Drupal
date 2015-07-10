e834410df0b5693a2faa29f5cf90cf33fed17656
1nickt <1nickt@users.noreply.github.com>
2015-07-10 14:17:25 -0700

    Initial commit

---
+++ lib/CMS/Drupal/Admin.pm
@@ -0,0 +1,30 @@
+package CMS::Drupal::Admin;
+
+# ABSTRACT: Provides the namespace for CMS::Drupal::Admin::* packages.
+
+use strict;
+
+# nothing here
+
+1; #return true
+
+__END__
+
+=pod
+
+=head1 NAME
+
+CMS::Drupal::Admin
+
+=head1 SYNOPSIS
+
+This an empty package.
+
+=head1 DESCRIPTION
+
+This is an empty package that only exists to provide the namespace for
+CMS::Drupal::Admin::* packages.
+
+Authors wishing to contribute packages for interacting administratively
+with a Drupal installation should create them under this namespace.
+
