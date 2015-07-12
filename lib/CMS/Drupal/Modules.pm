23a147d4c8f8852aa7a99e3ceebe0504553cca6c
1nickt <1nickt@users.noreply.github.com>
2015-07-12 15:53:22 -0700

    Add CMS::Drupal::Modules and a test for it

---
+++ lib/CMS/Drupal/Modules.pm
@@ -0,0 +1,61 @@
+package CMS::Drupal::Modules;
+
+# ABSTRACT: Provides the namespace for CMS::Drupal::Modules::* packages.
+
+use strict;
+
+# nothing here
+
+1; #return true
+__END__
+
+=head1 SYNOPSIS
+
+This an empty package.
+
+=head1 DESCRIPTION
+
+This is an empty package that only exists to provide the namespace for
+CMS::Drupal::Modules::* packages.
+
+Drupal has the concept of Modules much like Perl's. They are collections of code in libraries that can be used to extend the core Drupal installation. As in Perl, some are part of the core distribution, some are available from Drupal's equivalent of CPAN, and some may be completely custom to a local installation.
+
+=head1 NAMESPACES
+
+The Perl CMS::Drupal::* system takes no note of the distinction between types of Drupal modules. All are directly under the CMS::Drupal::Modules:: namespace.
+
+So if you want to contribute a Perl interface to the core Drupal module Poll, for example, your Perl package would be:
+
+  CMS::Drupal::Modules::Poll.pm
+  CMS::Drupal::Modules::Poll::SubModuleIfNeeded.pm
+
+With others, try to follow the naming hierarchy of the Drupal modules, although this is not always obvious.
+
+If you are working on Perl interfaces to Drupal's Commerce modules, for example, it's obvious that 
+
+  Commerce
+
+should map to 
+
+  CMS::Drupal::Modules::Commerce.pm
+
+And maybe somewhat obvious that other Commerce core modules such as
+
+  Customer
+  Order
+
+etc, should map to
+
+  CMS::Drupal::Modules::Commerce::Customer.pm
+  CMS::Drupal::Modules::Commerce::Order.pm
+
+and so on.
+
+But what about contributed modules, such as
+
+  Commerce Addressbook
+
+We'll just pretend that it's a Commerce core module and put your Perl package in with the rest of the Commerce interface:
+
+  CMS::Drupal::Modules::Commerce::AddressBook.pm
+
